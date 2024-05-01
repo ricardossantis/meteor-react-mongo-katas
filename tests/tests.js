6fdb9e22ed8915cf1e67f8fda1dc2c80d5b9d08e
ricardosantis <rsdsantis@gmail.com>
2024-05-01 17:05:22 -0300

    Main - Building challenges
    - fix - test imports

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,10 @@
 import '../katas/meteor_katas/level_1/meteor_1_example/method.test.js';
 // import '../katas/meteor_katas/level_2/meteor_1_1/method.test.js';
 import '../katas/meteor_katas/level_2/meteor_2_1/method.test.js';
-import '../katas/meteor_katas/level_4/meteor_3_1/method.test.js';
-import '../katas/meteor_katas/level_5/meteor_4_1/method.test.js';
-import '../katas/meteor_katas/level_6/meteor_5_1/method.test.js';
-import '../katas/meteor_katas/level_7/meteor_6_1/method.test.js';
+import '../katas/meteor_katas/level_3/meteor_3_1/method.test.js';
+import '../katas/meteor_katas/level_4/meteor_4_1/method.test.js';
+import '../katas/meteor_katas/level_5/meteor_5_1/method.test.js';
+import '../katas/meteor_katas/level_6/meteor_6_1/method.test.js';
 
 //MongoDb tests
 import '../katas/mongoDb_katas/level_1/mongoDb_1_example/method.test.js';
